9432a855ba64f9ae5badf923b5e5f66984776659
youssefr10 <youssef.radoui@gmail.com>
2026-01-14 14:14:28 +0100

    Update HelloController.java

--- src/main/java/ch/vaudoise/vaapi/controller/HelloController.java
+++ src/main/java/ch/vaudoise/vaapi/controller/HelloController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-import ch.vaudoise.vaapi.service.HelloService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
